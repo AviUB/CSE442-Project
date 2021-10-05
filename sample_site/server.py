4ca89d2d505ad8c34f1ccd939d058a40e71d9dd3
Robert <rbamm@buffalo.edu>
2021-10-05 14:16:51 -0400

    postgres database

--- sample_site/server.py
+++ sample_site/server.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 import sys
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def sample_page():
     return render_template("index.html")
+
+@app.route("/dbtest")
+def database_activity():
+    import psycopg2
+    db_config = os.environ["DATABASE_URL"] if "DATABASE_URL" in os.environ else "user=postgres password=password"
+
+    conn = psycopg2.connect(db_config, sslmode='require')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (username varchar, name varchar);")
+    cur.execute("INSERT INTO USERS (username, name) VALUES (%s, %s)", ("friendly", "green"))
+    cur.execute("SELECT * FROM users;")
+    data_from_db = cur.fetchone()
+    return"<p>"+data_from_db+"</p>"
+    
 
 if __name__=="__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
