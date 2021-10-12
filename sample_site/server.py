d41c964502d3fc3bbb3780c480858c10309caf7e
Robert <rbamm@buffalo.edu>
2021-10-11 21:45:54 -0400

    Stub functions for register page #19

--- sample_site/server.py
+++ sample_site/server.py
@@ -9,15 +9,20 @@ def sample_page():
     return render_template("index.html")
 
 @app.route("/create_account", methods=['GET','POST'])
-def create_account():
+def create_account_page():
     if request.method == 'POST':
-        #check for account creation
+        return create_account()
     else:
-        #render login page
-    return"<p>"+"This is not finished"+"</p>"
+        return register_page()
     
 
 if __name__=="__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
     print(port)
     app.run(host="0.0.0.0",port=port)
+
+def create_account():
+    return"<p>"+"This is not finished"+"</p>"
+
+def register_page():
+    return"<p>"+"This is not finished"+"</p>"
