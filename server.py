4c9ce2d2cd1a022f9c693894c61a823314e2318c
Robert <rbamm@buffalo.edu>
2021-10-12 23:29:46 -0400

    Server can recieve data and has db interaction #20 #21

--- server.py
+++ server.py
@@ -3,25 +3,45 @@ import sys
 import os
 import psycopg2
 
+db_config = os.environ["DATABASE_URL"] if "DATABASE_URL" in os.environ else "user=postgres password=password"
+
 def create_account(username, password):
-    return"<p>"+"You submitted a create account form: "+username+", "+password+"</p>"
+    conn = psycopg2.connect(db_config, sslmode='require')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users VALUES (username, password) VALUES (%s, %s)", (username, password))
+    conn.commit()
+    conn.close()
+    #TODO: Replace this with a redirect
+    return render_template("index.html", success=True)
 
 def valid_login(username, password):
     #if username in db already, return false, else true
-    return True
+    conn = psycopg2.connect(db_config, sslmode='require')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE username = %s;", (username, ))
+    account = cur.fetchone()
+    conn.close()
+    if account is None:
+        return True
+    else:
+        return False
 
 def invalid_account():
     return "<p>This is not a valid account: Username already in system!</p>"
 
 def initialize_db():
     #Create db tables
+    conn = psycopg2.connect(db_config, sslmode='require')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (username varchar, password varchar);")
+    conn.close
     return True
 
 app = Flask(__name__)
 
 @app.route("/")
 def sample_page():
-    return render_template("index.html")
+    return render_template("index.html", success=None)
 
 @app.route("/create_account", methods=['GET','POST'])
 def create_account_page():
