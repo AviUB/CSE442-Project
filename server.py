82c696ff46fbb89e84c8c5bfed37bb21625f0939
FortyFourForks <kittentacosl@gmail.com>
2021-10-18 19:01:17 -0400

    #33 changed port to 5000

--- server.py
+++ server.py
@@ -77,6 +77,6 @@ if __name__=="__main__":
     initialize_db()
     # Add dummy account
     create_account("u_name", "testPassword")
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
     print(port)
     app.run(host="0.0.0.0",port=port)
